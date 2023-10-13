f29ddaf2141c5e2c0b7ae25adf1a6b71b1d4e1e8
Reyansh Gupta <happygupta@aol.in>
2023-10-13 09:46:30 +0530

    Experiment 8
    Added user input and printing if the sequence is unsafe

--- exp8_bankers_algorithm.py
+++ exp8_bankers_algorithm.py
@@ -1,21 +1,21 @@
-p = 5  # Number of processes
-r = 3  # Number of resources
+p = int(input("Enter the number of processes: "))
+r = int(input("Enter the number of resources: "))
 
 # Max resources needed by each process
-max = [[7, 5, 3],
-       [3, 2, 2],
-       [9, 0, 2],
-       [2, 2, 2],
-       [4, 3, 3]]
+max = []
+print("Enter the maximum resources needed by each process:")
+for i in range(p):
+    resources = list(map(int, input().split()))
+    max.append(resources)
 
 # Allocated resources to each process
-alloc = [[1, 4, 2],
-         [2, 0, 3],
-         [3, 2, 2],
-         [2, 1, 1],
-         [1, 1, 2]]
+alloc = []
+print("Enter the allocated resources to each process:")
+for i in range(p):
+    resources = list(map(int, input().split()))
+    alloc.append(resources)
 
-available = [1, 3, 2]
+available = list(map(int, input("Enter the available resources: ").split()))
 
 proc_complete = [0] * p
 answer = [0] * p
@@ -32,20 +32,23 @@ for i in range(p):
 
 for x in range(p):
     for i in range(p):
-        if proc_complete[i] == 0: #processes hasn't completed
+        if proc_complete[i] == 0: #processes haven't completed
             flag = 0 #flag to check if need is less than available
             for j in range(r):
                 if need[i][j] > available[j]:
-                    flag = 1 #cant be allocated
+                    flag = 1 #can't be allocated
                     break
             if flag == 0:
                 answer[index] = i #add to safe sequence
                 index += 1
                 for k in range(r):
-                    available[k] += alloc[i][k] #add allocated to available when process is completed
+                    available[k] += alloc[i][k] #add allocated to available when the process is completed
                 proc_complete[i] = 1 #process completed
 
-print("Safe Sequence: ")
-for i in range(p - 1):
-    print("P", answer[i], "->", end=" ")
-print("P", answer[p - 1])
+if index < p:
+    print("No safe sequence found. The sequence is unsafe.")
+else:
+    print("Safe Sequence: ")
+    for i in range(p - 1):
+        print("P", answer[i], "->", end=" ")
+    print("P", answer[p - 1])
